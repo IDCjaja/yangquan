299e304bc0445fd98f61fa67336bb33b79a296de
renhaihuang <790226724@qq.com>
2020-03-14 23:09:59 +0800

    feat: share and crane.config

---
+++ crane.config.js
@@ -0,0 +1,49 @@
+const visPage = require('./crane/vis/index');
+module.exports = {
+  name: 'attention',
+
+  // i18n 配置
+  lang: {},
+  // 屏幕默认尺寸, 所有页面都继承该值，页面可单独设置
+  size: [1920, 1080],
+  requestSettings: {
+    baseURL: 'http://yqfw.cdyoue.com',
+  },
+  modules: {
+    '@byzanteam/vis-components': {
+      version: '../../vis-components',
+      style: {
+        type: 'normal',
+        path: 'dist/index.css',
+      },
+    },
+    '@byzanteam/graphite': {
+      version: 'latest',
+    },
+    '@byzanteam/carbonium': {
+      version: 'latest',
+      style: {
+        type: 'normal',
+        path: 'dist/carbonium.css',
+      },
+    },
+    'element-ui': {
+      version: 'latest',
+      style: {
+        type: 'normal',
+        path: 'lib/theme-chalk/index.css',
+      }
+    },
+    '@byzanteam/brick': {
+      version: 'latest',
+      style: {
+        type: 'injection',
+        path: 'src/theme/default.scss'
+      }
+    }
+  },
+  // 页面
+  pages: [
+    visPage,
+  ],
+};
---
+++ crane/share.js
@@ -0,0 +1,51 @@
+const chartTooltipOptions = {
+  $tooltip: {
+    $text: {
+      align: 'center',
+      baseline: 'middle',
+      fill: '#FFFFFF',
+      $size: 14,
+      $weight: 400
+    },
+    $notation: {
+      fill: '#007AFE',
+      name: 'circle-small',
+      $size: 14
+    }
+  },
+  $tooltipOptions: {
+    background: 'rgba(60, 71, 89, 0.9)',
+    $text: {
+      align: 'center',
+      baseline: 'middle',
+      fill: '#FFFFFF',
+      $size: 14,
+      $weight: 400
+    },
+    $title:{
+      align: 'center',
+      baseline: 'middle',
+      fill: '#FFFFFF',
+      $size: 14,
+      $weight: 400
+    },
+  }
+};
+exports.chartTooltipOptions = chartTooltipOptions;
+exports.normal_digital_style = {
+  $titleStyle: {
+    color: 'rgba(255, 255, 255)',
+    fontSize: '14px',
+    fontWeight: 500,
+  },
+  $suffixStyle: {
+    fontSize: '14px',
+    fontWeight: 400,
+  },
+  $digitalStyle: {
+    fontSize: '36px',
+    color: '#FFFFFF',
+    fontWeight: 400,
+    fontFamily: 'Oswald',
+  },
+};
