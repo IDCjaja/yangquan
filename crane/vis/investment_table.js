13557af408130289dcfab3d9764de280f5044291
idcjaja <838927992@qq.com>
2020-03-15 15:10:33 +0800

    feat: table style

--- crane/vis/investment_table.js
+++ crane/vis/investment_table.js
@@ -3,18 +3,17 @@ const {normal_digital_style} = require('../share');
 module.exports = {
   id: 'investment',
   component: '@byzanteam/vis-components/data-loader',
-  position: [63, 121],
+  position: [510, 676],
   exports: {
     results: 'results',
   },
-
   props: {
     url: "/v1/components/52ef7f5e-8046-4297-9671-cee40e05460c/data",
     method: 'get',
     $data: "[[0]]",
     $style: {
-      width: '194px',
-      height: '44px',
+      width: '900px',
+      height: '384px'
     },
   },
   children: [
